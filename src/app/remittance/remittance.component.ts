d5ed4ee57755ad0ffe566e56f6b94b305a29fd1c
samsundhar <samsundarjith@gmail.com>
2019-10-01 18:55:14 +0530

    added remittance component

--- src/app/remittance/remittance.component.ts
+++ src/app/remittance/remittance.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./remittance.component.scss']
 })
 export class RemittanceComponent implements OnInit {
+  TABLES: Table[] = [
+    { date: '02/12/2019', challanNo: 39, adminNo: 909, name: 'Alishan Ummer', course: 'BCA', ledger: 'REG', amount: 10000, mode: 'Cash' },
+    { date: '02/02/2019', challanNo: 40, adminNo: 910, name: 'Something', course: 'BBA', ledger: 'REG', amount: 15000, mode: 'Cash' }
+  ];
 
   constructor() { }
 
---
+++ src/app/remittance/test.ts
@@ -0,0 +1,10 @@
+export class Table {
+    date: string;
+    challanNo: number;
+    adminNo: number;
+    name: string;
+    course: string;
+    ledger: string;
+    amount: number;
+    mode: string;
+}
