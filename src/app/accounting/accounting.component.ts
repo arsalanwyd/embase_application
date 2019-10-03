bc380fe2c35c78fb305514bb99e6633d22e3ae9f
samsundhar <samsundarjith@gmail.com>
2019-10-03 12:19:24 +0530

    added accounting component

--- src/app/accounting/accounting.component.ts
+++ src/app/accounting/accounting.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accounting.component.scss']
 })
 export class AccountingComponent implements OnInit {
+  selectAction=false;
+  private fieldArray: Array<any> = [];
+    private newAttribute: any = {};
 
   constructor() { }
 
   ngOnInit() {
   }
+  addFieldValue() {
+    this.fieldArray.push(this.newAttribute)
+    this.newAttribute = {};
+}
+
+deleteFieldValue(index) {
+    this.fieldArray.splice(index, 1);
+}
+onSelectActionClick($event)
+{
+  this.selectAction=true;
+  console.log("Select Action Clicked",$event);
+}
 
 }
